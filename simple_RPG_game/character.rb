af924ca159ba1afd245759fe0c84f97224ef41ef
ShotoMatsubara <shoto0628matsubara@gmail.com>
2024-07-21 21:09:30 +0900

    キャラクターの基本となるスーパークラスを書く

---
+++ simple_RPG_game/character.rb
@@ -0,0 +1,23 @@
+# ゲーム内の全てのキャラクター（プレイヤーと敵）の基本となるクラス
+
+class Character
+  # attr_reader はインスタンス変数を外部から読み取り可能にするメソッドを自動的に定義
+  attr_reader :name, :hp, :attack
+
+  def initialize(name, hp, attack)
+    @name = name
+    @hp = hp
+    @attack = attack
+  end
+
+  def take_damage(damage_received)
+    @hp -= damage_received
+    @hp = 0 if @hp < 0
+    puts "#{@name}は#{damege_received}ダメージを受けた。残りHP: #{@hp}"
+  end
+
+  # メソッド名の末尾の?は、真偽値を返すメソッドであることを示すRubyの慣習
+  def alive?
+    @hp > 0
+  end
+end
