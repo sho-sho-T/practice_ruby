e06781c2a5683f7da3266ddb94eb46c80fb4f2a4
ShotoMatsubara <shoto0628matsubara@gmail.com>
2024-07-21 21:26:24 +0900

    Enemyクラスを追加

---
+++ simple_RPG_game/enemy.rb
@@ -0,0 +1,10 @@
+require_relative 'character'
+
+class Enemy < Character
+  # 一旦Characterクラスの継承したメソッドをそのまま使うのでなんもメソッドを定義しない
+  # 将来的に追加するかもっていうメソッドを下にメモしとく
+
+  # def special_attack
+  #  # 敵の特殊攻撃
+  # end
+end
