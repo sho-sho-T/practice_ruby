cb8a6b787754eb170799f2210d8f9c191a0738f4
ShotoMatsubara <shoto0628matsubara@gmail.com>
2024-08-31 22:10:59 +0900

    問３を解いた

--- basic_learning/exercises01/my_answer.rb
+++ basic_learning/exercises01/my_answer.rb
@@ -14,22 +14,6 @@ end
 
 puts find_maximum(numbers)
 
-# --------- claude採点 ------------------------
-# 問題１
-# 90点
-#
-# 正しい点:
-
-# find_maximum メソッドを正しく定義しています。
-# 配列をループで処理するために each メソッドを使用しています。
-# 条件分岐を使って最大値を更新しています。
-# 最終的に最大値を返しています。
-
-# 改善できる点:
-
-# 初期値の設定: maximum_num = 0 としていますが、配列に負の数が含まれる場合に正しく動作しません。
-# ------------------------------------------------
-
 # 問題２：この find_maximum メソッドを使って、配列内の最小値を見つけるメソッド find_minimum を作成してみてください。
 def find_minimum(arr)
   return if arr.empty?
@@ -44,3 +28,16 @@ def find_minimum(arr)
 end
 
 puts find_minimum(numbers)
+
+# 問題３：両方のメソッドを使って、配列内の最大値と最小値の差（範囲）を計算するメソッドを作成してみてください。
+
+def get_range(arr)
+  return if arr.empty?
+
+  maximum_num = find_maximum(arr)
+  minimum_num = find_minimum(arr)
+
+  maximum_num - minimum_num
+end
+
+puts get_range(numbers)
