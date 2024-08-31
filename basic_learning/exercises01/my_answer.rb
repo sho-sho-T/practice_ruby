f37c90c60a5face46f0802f9e4c1ac0b32e97286
ShotoMatsubara <shoto0628matsubara@gmail.com>
2024-08-31 21:56:13 +0900

    問１、２をやる

--- basic_learning/exercises01/my_answer.rb
+++ basic_learning/exercises01/my_answer.rb
@@ -1,14 +1,46 @@
-# 問題: 配列内の最大値を見つける
+# 問題１: 配列内の最大値を見つける
 
-# 以下の配列が与えられています
 numbers = [3, 7, 2, 8, 5, 1, 9, 4, 6]
 
-# この配列の中から最大値を見つけ、出力するメソッドを作成してください
-# ヒント: 配列のメソッドは使わず、ループを使って実装してみましょう
-
 def find_maximum(arr)
-  # ここにコードを書いてください
+  maximum_num = 0
+
+  arr.each do |num|
+    maximum_num = num if num > maximum_num
+  end
+
+  maximum_num
 end
 
-# テスト
-puts find_maximum(numbers) # 期待される出力: 9
+puts find_maximum(numbers)
+
+# --------- claude採点 ------------------------
+# 問題１
+# 90点
+#
+# 正しい点:
+
+# find_maximum メソッドを正しく定義しています。
+# 配列をループで処理するために each メソッドを使用しています。
+# 条件分岐を使って最大値を更新しています。
+# 最終的に最大値を返しています。
+
+# 改善できる点:
+
+# 初期値の設定: maximum_num = 0 としていますが、配列に負の数が含まれる場合に正しく動作しません。
+# ------------------------------------------------
+
+# 問題２：この find_maximum メソッドを使って、配列内の最小値を見つけるメソッド find_minimum を作成してみてください。
+def find_minimum(arr)
+  return if arr.empty?
+
+  minimum_num = arr[0]
+
+  arr.each do |num|
+    minimum_num = num if num < minimum_num
+  end
+
+  minimum_num
+end
+
+puts find_minimum(numbers)
